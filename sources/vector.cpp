940fa25716d27077bd2fffc21e8ddeffb67e200f
Link-God <32190920+Link-God@users.noreply.github.com>
2018-03-16 11:41:36 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -5,55 +5,126 @@
 
 vector_t::vector_t()
 {
+	elements_ = nullptr;
+	size_ = 0;
+	capacity_ = 0;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
+	size_ = other.size_;
+	capacity_ = other.capacity_;
+	elements_ = new int[capacity_];
+	for (size_t i = 0; i < size_; i++) {
+		elements_[i] = other.elements_[i];
+	}
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
+	delete[] elements_;
+	size_ = other.size_;
+	capacity_ = other.capacity_;
+	elements_ = new int[capacity_];
+	for (size_t i = 0; i < size_; i++) {
+		elements_[i] = other.elements_[i];
+	}
 	return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	return false;
+	bool success=false;
+	if (size_ == other.size_ && capacity_ == other.capacity_) {
+		for (size_t i = 0; i < size_; i++) {
+			if (elements_[i] != other.elements_[i]){
+				success = false;
+				break;
+			}
+			else {
+				success = true;
+			}
+		}
+	}
+	if (success) {
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 vector_t::~vector_t()
 {
+	delete[] elements_;
 }
 
 std::size_t vector_t::size() const
 {
-    return 0;
+	return size_;
 }
 
 std::size_t vector_t::capacity() const
 {
-    return 0;
+	return capacity_;
 }
 
 void vector_t::push_back(int value)
 {
+	if (capacity_ == 0) {
+		size_ = 1;
+		capacity_ = 1;
+		elements_ = new int [capacity_];
+		elements_[0] = value;
+	}
+	else {
+		int * mas;
+		mas = new int[capacity_];
+		for (size_t i = 0; i < size_; i++) {
+			mas[i] = elements_[i];
+		}
+		delete[] elements_;
+		if (capacity_ == size_) {
+			capacity_ = 2 * capacity_;
+		}
+		elements_ = new int[capacity_];
+		for (size_t i = 0; i < size_; i++) {
+			elements_[i] = mas[i];
+		}
+		delete[] mas;
+		elements_[size_] = value;
+		size_++;
+	}
 }
 
 void vector_t::pop_back()
 {
+	size_--;
+	if (size_ == 0){
+		capacity_ = 1;
+	}
+	else {
+		if (size_ * 4 == capacity_) {
+			capacity_ = capacity_ / 2;
+		}
+	}
 }
 
 int & vector_t::operator [](std::size_t index)
 {
-	return elements_[0];
+	return elements_[index];
 }
 
 int vector_t::operator [](std::size_t index) const
 {
-	return 0;
+	return elements_[index];
 }
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs)
 {
-	return true;
+	bool success = true;
+	if (lhs == rhs) {
+		success = !success;
+	}
+	return success;
 }
