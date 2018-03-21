bed53cfc175493dfcce124efeff7abfbc739e528
Link-God <32190920+Link-God@users.noreply.github.com>
2018-03-22 01:32:39 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,27 +1,148 @@
 #include <iostream>
-
+tamplate <typename T >
 class vector_t
 {
 private:
-	int * elements_;
+	T * elements_;
 	std::size_t size_;
 	std::size_t capacity_;
 public:
-	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	~vector_t();
+	vector_t()
+	{
+		elements_ = nullptr;
+		size_ = 0;
+		capacity_ = 0;
+	}
+	vector_t(vector_t<T> const & other)
+	{
+		size_ = other.size_;
+		capacity_ = other.capacity_;
+		elements_ = new T[capacity_];
+		for (size_t i = 0; i < size_; i++) {
+			elements_[i] = other.elements_[i];
+		}
+	}
+	vector_t<T> & operator =(vector_t<T> const & other)
+	{
+		if(this != &other){
+			delete[] elements_;
+			size_ = other.size_;
+			capacity_ = other.capacity_;
+			elements_ = new T[capacity_];
+			for (size_t i = 0; i < size_; i++) {
+				elements_[i] = other.elements_[i];
+			}
+		}
+		return *this;
+	}
+	~vector_t()
+	{
+		delete[] elements_;
+	}
 
-	std::size_t size() const;
-	std::size_t capacity() const;
+	std::size_t size() const
+	{
+		return size_;
+	}
+	std::size_t capacity() const
+	{
+		return capacity_;
+	}
 
-	void push_back(int value);
-	void pop_back();
+	void push_back(T value)
+	{
+	
+		if (capacity_ == 0) {
+		size_ = 1;
+		capacity_ = 1;
+		elements_ = new int [capacity_];
+		elements_[0] = value;
+		}
+		else {
+			int * mas;
+			if (capacity_ == size_) {
+				capacity_ = 2 * capacity_;
+			}
+			mas = new int[capacity_];
+			for (size_t i = 0; i < size_; i++) {
+				mas[i] = elements_[i];
+			}
+			delete[] elements_;
+			elements_ = mas;
+			elements_[size_] = value;
+			size_++;
+		}
+	}
+	void pop_back()
+	{
+		if (size_ == 0) return;
+		size_--;
+		if (size_ ==0 || size_ * 4 == capacity_) {
+			int * mas;
+			capacity_ = capacity_ / 2;
+			mas = new int[capacity_];
+			for (size_t i = 0; i < size_; i++) {
+				mas[i] = elements_[i];
+			}
+			delete[] elements_;
+			elements_ = mas;
+		}
+	}
 
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
+	T & operator [](std::size_t index)
+	{
+		if(index >= size_){
+			throw std::out_of_range("Invalid range") 
+		}
+		return elements_[index];
+	}
+	T operator [](std::size_t index) const
+	{
+		if(index >= size_){
+			throw std::out_of_range("Invalid range") 
+		}
+		return elements_[index];
+	}
+	boop at(std::size_t index)
+	{
+		bool success = true
+		try{
+			this[index];
+		}
+		catch(std::out_of_range){
+			success = false ;
+		}
+		return success ;
+	}
 
-	bool operator ==(vector_t const & other) const;
+	bool operator ==(vector_t<T> const & other) const
+	{
+		bool success=false;
+		if (size_ == other.size_ && capacity_ == other.capacity_) {
+			for (size_t i = 0; i < size_; i++) {
+				if (elements_[i] != other.elements_[i]){
+					success = false;
+					break;
+				}
+				else {
+					success = true;
+				}
+			}
+		}
+		if (success) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 };
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
+bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs)
+{
+	bool success = true;
+	if (lhs == rhs) {
+		success = !success;
+	}
+	return success;
+}
