36860c7d4713c3e2d3d9cfda5a083ed587b13ce3
Yury <32190920+Link-God@users.noreply.github.com>
2018-03-27 22:46:08 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -6,5 +6,47 @@
 TEST_CASE("creating tree")
 {
 	tree_t tree;
-	REQUIRE( tree.root_ == nullptr );
+	REQUIRE( tree.root() == nullptr );
+}
+
+TEST_CASE("conlose")
+{
+	// необходимый код для выполнение консольных команд 
+	/*
+		std::ostringstream ostream;
+		Operator(ostream , tree );
+	*/
+	REQUIRE( 0 == 0 );
+}
+
+TEST_CASE("insert_and_print") {
+	tree_t  tree;
+	tree.insert(2);
+	tree.insert(1);
+	tree.insert(3);
+	
+	std::string out {
+		"----4\n"
+		"--2\n"
+		"----2\n"
+	};
+	std::ostringstream ostream;
+	tree.print(ostream , tree.root());
+	REQUIRE(stream.str() == out);
+}
+
+TEST_CASE("find")
+	{
+	tree_t tree;
+	tree.insert(2);
+	tree.insert(1);
+	tree.insert(3);
+	tree.insert(6);
+	tree.insert(9);
+	tree.insert(7);
+	
+	REQUIRE(tree.find(5) == false);
+	REQUIRE(tree.find(8) == false);
+	REQUIRE(tree.find(9) == true);	
+	REQUIRE(tree.find(7) == true);
 }
